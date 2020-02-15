083f57b144bbee99b64e7d37e28e8b66fae3fe90
kimarinov <58824713+kimarinov@users.noreply.github.com>
2020-02-16 00:35:53 +0200

    sunk

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html>
 <head>
 	<title>Project</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<form method="post" action="project1.php">
+	<form method="post" action="php/project1.php">
 		<p>Input the figthers "a", "b", "g"</p> <input type="text" name="input_data_oponents">
 		<p>Master(Healt, Atack, Resistance)</p> <input type="text" name="input_data">
 		<input type="submit" name="">
